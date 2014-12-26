769645903240c99780ab2cfcd398909337e93012
Vincent Lark <vincent.lark@gmail.com>
2014-12-26 18:22:55 +0100

    now use underscore-query, still random sort

--- index.js
+++ index.js
@@ -14,8 +14,7 @@
 
     function Reactive (db) {
 
-        this.db = new loki(db || 'test.json');
-        this.voices = this.db.addCollection('voices');
+        this.db  = new underscoreQueryInterface();
 
         this.context = this.getContext();
     }
@@ -34,7 +33,7 @@
 
         var sounds = [];
 
-        this.voices.data.forEach(function (obj) {
+        this.db.all().forEach(function (obj) {
             if (obj.sound) {
                 sounds.push(obj.sound);
                 if (obj.reponse) {
@@ -43,42 +42,46 @@
             }
         });
 
-        this.bufferLoader = new BufferLoader(this.context, sounds, function (bufferSources) {
-        });
+        this.bufferLoader = new BufferLoader(this.context, sounds, function (bufferSources) {});
 
         this.bufferLoader.load(function (url, buffer) {
-            self.voices.find({ sound: url }).forEach(function(obj){
+            self.db.find({ sound: url }).forEach(function(obj){
                 obj.buffer = buffer;
             });
-            self.voices.find({ response: url }).forEach(function(obj){
+            self.db.find({ response: url }).forEach(function(obj){
                 obj.response_buffer = buffer;
             });
         });
     };
 
     Reactive.prototype.add = function (data) {
-        return this.voices.insert(data);
+
+        data._playedAt = 0;
+        return this.db.add(data);
     };
 
-    Reactive.prototype.query = function (criteria) {
-        var query = function (obj) {
-            var retain = false;
-            for (prop in criteria) {
-                if (obj[prop] === criteria[prop]) {
-                    retain = true;
-                }
-            }
-            return retain;
+    Reactive.prototype.query = function (criteria, playedHistory) {
+
+        criteria = [
+            { $or: criteria }
+        ];
+
+        criteria.push({
+            _playedAt: { $lt: Date.now() - 1000 }
+        });
+
+        if (playedHistory) {
+            criteria.push({ $not: { sound: { $in: playedHistory }}});
         }
-        var results = this.voices.where(query);
-        return results.length > 0 ? results[~~(Math.random() * results.length)] : null;
+
+        return this.db.query(criteria);
     };
 
-    Reactive.prototype.queryplay = function (criteria) {
-        var found = this.query(criteria);
-        console.log(found);
+    Reactive.prototype.queryplay = function (criteria, playedHistory) {
 
         var self = this;
+
+        var found = this.query(criteria, playedHistory);
 
         if (found && found.buffer) {
 
@@ -87,32 +90,80 @@
             // We have a response buffer
             if (found.response_buffer) {
 
-                chain(source, function () { response.start(0); });
+                chain(source, function () { self.play(found.response_buffer); });
 
             // Rule response's is an object
             } else if (typeof found.response === 'object') {
 
-                chain(source, function () { self.queryplay(found.response); });
+                playedHistory = playedHistory || [];
+                playedHistory.push(found.sound);
+
+                chain(source, function () { self.queryplay(found.response, playedHistory); });
 
             }
 
-            source.start(0);
+            self.play(source, found);
         }
+
         return found;
     };
 
     Reactive.prototype.bufferSource = function(buffer) {
+
         var source = this.context.createBufferSource();
         source.connect(this.context.destination);
         source.buffer = buffer;
+
         return source;
     };
 
+    Reactive.prototype.play = function(source, item) {
+
+        if (item) item._playedAt = Date.now();
+        if (source) source.start(0);
+    };
+
     function chain (source, func) {
+
         source.onended = function () {
             setTimeout(func, 300);
         };
     }
+
+
+    //////////////////////////////////////////////
+
+
+    function underscoreQueryInterface () {
+
+        var db = [];
+
+        this.all = function () {
+
+            return db;
+        };
+
+        this.find = function (criteria) {
+
+            return _.where(db, criteria) || [];
+        };
+
+        this.add = function (data) {
+
+            return db.push(data);
+        };
+
+        this.query = function (criteria) {
+
+            var results = _.query(db, { $and: criteria });
+
+            return results.length > 0 ? results[~~(Math.random() * results.length)] : null;
+        };
+    };
+
+
+    //////////////////////////////////////////////
+
 
     // Node.js
     if (typeof module !== 'undefined' && module.exports) {
@@ -128,5 +179,6 @@
     else {
         root.Reactive = Reactive;
     }
+
 }());
 
